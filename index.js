ef3b1623790c86795e1eb123ea6e31f2c7b44b7a
Falkirks <bpnoreply@gmail.com>
2015-04-14 12:45:01 -0700

    Version 0.0.3, included JQuery directly

--- index.js
+++ index.js
@@ -11,18 +11,15 @@ p.run = function(uri, cb){
         ph.createPage(function (page) {
             page.open(uri, function (status) {
                 cb({state: "LOADED"});
-                page.includeJs("http://code.jquery.com/jquery-2.1.3.min.js", function() { // This should be updated to a local file to avoid need for internet
-                    cb({state: "DOWNLOADED_LIBS"});
-                    page.includeJs(__dirname + "/include.js", function () {
-                        cb({state: "PROCESSED"});
-                        page.evaluate(function () {
-                            var doc = document.documentElement.innerHTML;
-                            doc = doc.substring(doc.indexOf("<body>") + 6);
-                            return doc.substring(0, doc.indexOf("<script"));
-                        }, function (result) {
-                            cb({state: "DONE", page: result});
-                            ph.exit();
-                        });
+                page.includeJs(__dirname + "/include.js", function () {
+                    cb({state: "PROCESSED"});
+                    page.evaluate(function () {
+                        var doc = document.documentElement.innerHTML;
+                        doc = doc.substring(doc.indexOf("<body>") + 6);
+                        return doc.substring(0, doc.indexOf("<script"));
+                    }, function (result) {
+                        cb({state: "DONE", page: result});
+                        ph.exit();
                     });
                 });
             });
